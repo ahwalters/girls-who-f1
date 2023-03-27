9f012cc5331ef045f77125ff2c59e89009c3fa37
Alana Walters <lana@Alanas-MacBook-Air.local>
2023-03-26 22:23:38 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <button>Home</button>
+        <button>About</button>
+        <button>Follow</button>
+      </div>
+    </div>
+  );
+}
