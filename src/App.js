d1b495d48a3817af6b0ac57bbcec7170d52eed38
Alana Walters <lana@Alanas-MacBook-Air.local>
2023-03-26 22:50:21 -0400

    Rendered some elements to the DOM

--- src/App.js
+++ src/App.js
@@ -8,6 +8,62 @@ export default function App() {
         <button>About</button>
         <button>Follow</button>
       </div>
+      <div>
+        <img src={require("./Assets/Logo.png")} alt="logo" width="300"/>
+        <h1>Girls Who F1</h1>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <img src={require("./Assets/TrackMap.jpg")} width="275"/>
+      </div>
+      <div>
+        <h2>Follow our Social Media!</h2>
+        <div>
+          <p>Spotify, Apple Music, Podcast, Tiktok, Twitter, Instagtram etc. icons w/ links</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src={require("./Assets/Kenny1.jpg")} width="100"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div>
+          <img src={require("./Assets/Hunter.jpg")} width="100"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div>
+          <img src={require("./Assets/Raevin.jpg")} width="100"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+      </div>
+      <div>
+          <h3>All About F1</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <img src={require("./Assets/Screens.jpg")} width="100"/>
+        </div>
+        <div>
+          <h4>Contact Us</h4>
+          <div>
+            <p>phone number</p>
+            <p>email address</p>
+            <p>po box?</p>
+          </div>
+          <div>
+            <form>
+              <label>Name
+              <input type='text' placeholder='name'/>
+              </label>
+              <label>Email
+              <input type='text' placeholder='email'/>
+              </label>
+              <label>Message
+              <input type='text' placeholder='message'/>
+              </label>
+              <button>Submit</button>
+            </form>
+          </div>
+          <div className="Footer">Footer</div>
+        </div>
     </div>
   );
 }
